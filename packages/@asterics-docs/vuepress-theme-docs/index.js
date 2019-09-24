e0bb5e894a7d3f72073fe281ee542093ad5e32f7
Alija Sabic <sabic.alija@gmail.com>
2019-09-25 01:16:08 +0200

    Update @asterics-docs/vuepress-theme-docs
    * vuepress-theme-docs
      * add markdown-it plugins and twemoji emojis
      * add bootstrap and bootstrap-vue
      * add fontawesome icons
      * add plugins
        * @vuepress/plugin-back-to-top
        * @vuepress/plugin-medium-zoom

--- packages/@asterics-docs/vuepress-theme-docs/index.js
+++ packages/@asterics-docs/vuepress-theme-docs/index.js
@@ -1,3 +1,34 @@
-module.exports = {
-  extend: "@vuepress/theme-default"
+const twemoji = require("twemoji");
+
+module.exports = (themeConfig, ctx) => {
+  const markdownConfiguration = md => {
+    md.set({ false: true, typographer: true, linkify: true });
+    md.use(require("markdown-it-sub"));
+    md.use(require("markdown-it-sup"));
+    md.use(require("markdown-it-footnote"));
+    md.use(require("markdown-it-deflist"));
+    md.use(require("markdown-it-abbr"));
+    md.use(require("markdown-it-emoji"));
+    md.use(require("markdown-it-checkbox"));
+    md.use(require("markdown-it-imsize"), { autofill: true });
+    md.use(require("markdown-it-kbd"));
+
+    md.renderer.rules.emoji = function(token, idx) {
+      return twemoji.parse(token[idx].content, {
+        base: "https://twemoji.maxcdn.com/2/",
+        ext: ".svg",
+        folder: "svg"
+      });
+    };
+  };
+
+  const plugins = ["@vuepress/medium-zoom", "@vuepress/back-to-top"];
+
+  const config = {
+    extend: "@vuepress/theme-default",
+    extendMarkdown: markdownConfiguration,
+    plugins
+  };
+
+  return config;
 };
